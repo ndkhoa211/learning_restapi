77bf451dc6481b3789e0b13f9243a7b8b85ea7e9
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-09-11 21:07:47 +0800

    60 write 2 GET requests to get specific store and its item

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 stores = [
     {
         "name": "My Store",
-        "item": [
+        "items": [
             {
                 "name": "Chair",
                 "price": 15.99
@@ -25,3 +25,30 @@ def create_store():
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store, 201
+    return {"message": "Store not found"}, 404
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404

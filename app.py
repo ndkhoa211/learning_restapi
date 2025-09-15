68820c1e306b4673e1ed1920f07544fecf6194d7
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-09-15 23:20:49 +0800

    91 set up flask-jwt-extended with our app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+import secrets
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 
 from db import db
 import models
@@ -26,6 +28,11 @@ def create_app(db_url=None):
     db.init_app(app)
 
     api = Api(app)
+
+    #app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
+    #app.config["JWT_SECRET_KEY"] = "167636583323287945340709459545609432193"
+    app.config["JWT_SECRET_KEY"] = "khoa"
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()

3660bca094cf55b7069da2b9d8a9b278bf2e5a61
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-09-16 18:10:20 +0800

    97 add logout POST endpoint

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
+from blocklist import BLOCKLIST
 import models
 
 from resources.item import blp as ItemBlueprint
@@ -35,6 +36,20 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "khoa"
     jwt = JWTManager(app)
 
+    
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
 
     @jwt.additional_claims_loader
     def add_claims_to_jwt(identity):
@@ -42,7 +57,6 @@ def create_app(db_url=None):
         if identity == 1:
             return {"is_admin": True}
         return {"is_admin": False}
-
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):

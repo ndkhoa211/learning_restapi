82a0e2cfbc026b8bb58ac7cb5cece2a13b51a29f
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-09-16 17:12:06 +0800

    95 protect endpoints by requiring a jwt access_token

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 
@@ -34,6 +34,34 @@ def create_app(db_url=None):
     #app.config["JWT_SECRET_KEY"] = "167636583323287945340709459545609432193"
     app.config["JWT_SECRET_KEY"] = "khoa"
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
 
     with app.app_context():
         db.create_all()
